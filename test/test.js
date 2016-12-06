3da59f913dc793fc6969721bb8a8c25f10780edd
Justin Beckwith <beckwith@google.com>
2016-12-06 09:53:03 -0800

    add flex health check hack

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const https = require('https');
 const fs = require('fs');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
+
 describe('yes', () => {
   it('should perform the 301 for an http request', (done) => {
     
@@ -30,24 +32,11 @@ describe('yes', () => {
     let app = express();
     app.use(yes());
     app.get('/test', (req, res) => {
-      console.log('in test?');
       res.sendStatus(200);
     });
 
-    // server the app over https
-    var secureServer = https.createServer({
-      key: fs.readFileSync('./test/certs/server.key'),
-      cert: fs.readFileSync('./test/certs/server.crt'),
-      ca: fs.readFileSync('./test/certs/ca.crt'),
-      requestCert: true,
-      rejectUnauthorized: false
-    }, app)
-    .listen('8443', () => {
-      console.log("Secure Express server listening on port 8443");
-    });
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
     // verify the request returns the right header when using https
+    let server = createSecureServer(app);
     request('https://localhost:8443')
       .get('/test')
       .expect('Strict-Transport-Security', 'max-age=86400; includeSubDomains')  
@@ -56,5 +45,36 @@ describe('yes', () => {
         if (err) throw err;
         done();
       });;
+  }).timeout(60000);
+
+  it('should ignore flex health checks // HACK HACK HACK', (done) => {
+    
+    // configure a minimal web server with the defaults
+    let app = express();
+    app.use(yes());
+    app.get('/_ah/health', (req, res) => {
+      res.sendStatus(200);
+    });
+
+    // verify the request returns a 200 for health checks
+    request(app)
+      .get('/_ah/health')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        done();
+      });
   });
 });
+
+
+function createSecureServer(app) {
+  // server the app over https
+  return https.createServer({
+    key: fs.readFileSync('./test/certs/server.key'),
+    cert: fs.readFileSync('./test/certs/server.crt'),
+    ca: fs.readFileSync('./test/certs/ca.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+  }, app).listen('8443');
+}
