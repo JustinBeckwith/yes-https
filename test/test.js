a637eb56b2fdccf317037918230b64a2bf28cd92
Justin Beckwith <justin.beckwith@gmail.com>
2019-04-07 12:36:34 -0700

    build: use XO for linting (#27)

--- test/test.js
+++ test/test.js
@@ -1,86 +1,91 @@
-const yes = require('../lib/index.js');
-const express = require('express');
-const request = require('supertest');
 const https = require('https');
 const fs = require('fs');
+const express = require('express');
+const request = require('supertest');
+const yes = require('../lib');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED=0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 describe('yes', () => {
+	it('should perform the 301 for an http request', done => {
+		// Configure a minimal web server with the defaults
+		const app = express();
+		app.use(yes());
+		app.get('/test', (req, res) => {
+			res.sendStatus(200);
+		});
 
-  it('should perform the 301 for an http request', (done) => {
+		// Verify the request returns a 301
+		request(app)
+			.get('/test')
+			.expect(301)
+			.end(err => {
+				if (err) {
+					throw err;
+				}
 
-    // configure a minimal web server with the defaults
-    const app = express();
-    app.use(yes());
-    app.get('/test', (req, res) => {
-      res.sendStatus(200);
-    });
+				done();
+			});
+	});
 
-    // verify the request returns a 301
-    request(app)
-      .get('/test')
-      .expect(301)
-      .end((err, res) => {
-        if (err) throw err;
-        done();
-      });
-  });
+	it('should use the correct defaults', done => {
+		// Configure a minimal web server with the defaults
+		const app = express();
+		app.use(yes());
+		app.get('/test', (req, res) => {
+			res.sendStatus(200);
+		});
 
-  it('should use the correct defaults', (done) => {
+		// Verify the request returns the right header when using https
+		const server = createSecureServer(app);
+		request('https://localhost:8443')
+			.get('/test')
+			.expect('Strict-Transport-Security', 'max-age=86400; includeSubDomains')
+			.expect(200)
+			.end(err => {
+				if (err) {
+					throw err;
+				}
 
-    // configure a minimal web server with the defaults
-    const app = express();
-    app.use(yes());
-    app.get('/test', (req, res) => {
-      res.sendStatus(200);
-    });
+				server.close();
+				done();
+			});
+	}).timeout(60000);
 
-    // verify the request returns the right header when using https
-    const server = createSecureServer(app);
-    request('https://localhost:8443')
-      .get('/test')
-      .expect('Strict-Transport-Security', 'max-age=86400; includeSubDomains')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        server.close();
-        done();
-      });;
-  }).timeout(60000);
+	it('should ignore filtered requests', done => {
+		// Configure a minimal web server with the defaults
+		const app = express();
+		app.use(yes({
+			ignoreFilter: req => {
+				return (req.url.indexOf('/_ah/health') > -1);
+			}
+		}));
 
-  it('should ignore filtered requests', (done) => {
+		app.get('/_ah/health', (req, res) => {
+			res.sendStatus(200);
+		});
 
-    // configure a minimal web server with the defaults
-    const app = express();
-    app.use(yes({
-      ignoreFilter: (req) => {
-        return (req.url.indexOf('/_ah/health') > -1);
-      }
-    }));
+		// Verify the request returns a 200 for health checks
+		request(app)
+			.get('/_ah/health')
+			.expect(200)
+			.end(err => {
+				if (err) {
+					throw err;
+				}
 
-    app.get('/_ah/health', (req, res) => {
-      res.sendStatus(200);
-    });
-
-    // verify the request returns a 200 for health checks
-    request(app)
-      .get('/_ah/health')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err;
-        done();
-      });
-  });
+				done();
+			});
+	});
 });
 
 function createSecureServer(app) {
-  // server the app over https
-  return https.createServer({
-    key: fs.readFileSync('./test/certs/server.key'),
-    cert: fs.readFileSync('./test/certs/server.crt'),
-    ca: fs.readFileSync('./test/certs/ca.crt'),
-    requestCert: true,
-    rejectUnauthorized: false
-  }, app).listen('8443');
+	// Server the app over https
+	return https.createServer({
+		key: fs.readFileSync('./test/certs/server.key'),
+		cert: fs.readFileSync('./test/certs/server.crt'),
+		ca: fs.readFileSync('./test/certs/ca.crt'),
+		requestCert: true,
+		rejectUnauthorized: false
+	}, app).listen('8443');
 }
