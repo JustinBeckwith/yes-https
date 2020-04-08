5ccbe34347dfc9b296eef2723b63f13c98aa3f80
Justin Beckwith <beckwith@google.com>
2020-04-08 10:12:38 -0700

    build!: require node.js 10x and up (#37)
    BREAKING CHANGE: Drops support for node.js 6 and node.js 8.

--- example/app.js
+++ example/app.js
@@ -6,8 +6,8 @@ const app = express();
 // Use the yes-https connect middleware.  Note - this will only work if NODE_ENV is set to production.
 app.use(yes());
 
-app.get('/', (req, res) => {
-	res.end('Thanks for checking it out!');
+app.get('/', (request, response) => {
+	response.end('Thanks for checking it out!');
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
