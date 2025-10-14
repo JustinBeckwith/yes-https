7416756340080f60a4a5929b536c476ff0385c3e
Justin Beckwith <justin.beckwith@discordapp.com>
2025-10-14 09:38:51 -0700

    build: use biome for formatting and fix tests (#125)

--- example/app.js
+++ example/app.js
@@ -7,11 +7,11 @@ const app = express();
 // Use the yes-https connect middleware.  Note - this will only work if NODE_ENV is set to production.
 app.use(yes());
 
-app.get('/', (request, response) => {
-	response.end('Thanks for checking it out!');
+app.get('/', (_request, response) => {
+  response.end('Thanks for checking it out!');
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-	console.log('App listening on port %s', server.address().port);
-	console.log('Press Ctrl+C to quit.');
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
 });
